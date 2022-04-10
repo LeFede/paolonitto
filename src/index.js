d2a3cadf6e0add1e618deee80d3f28c5be481294
LeFede <lefedeok@gmail.com>
2022-04-10 21:13:35 +0200

    2

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root')
 
-render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  container
-);
+render(<App />, container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
