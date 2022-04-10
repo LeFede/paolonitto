bfb12c20a02764e94ab711169afde3458c14b396
LeFede <lefedeok@gmail.com>
2022-04-10 21:21:10 +0200

    5

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const container = document.getElementById('root')
 // );
 
 const root = createRoot(container)
-root.render(<App />)
+root.render(<StrictMode><App /></StrictMode>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
