125641bb384662e61257818636f788a725fa9305
LeFede <lefedeok@gmail.com>
2022-04-10 21:32:58 +0200

    fix?

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('root')
+const container = document.querySelector('#root')
 
-// render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-//   container
-// );
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  container
+);
 
-const root = createRoot(container)
-root.render(<StrictMode><App /></StrictMode>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
