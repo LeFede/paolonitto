2413a1623117d25e3ca5ecabbd6e20a26f63b9c4
LeFede <lefedeok@gmail.com>
2022-04-15 14:15:51 +0200

    Removed boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.querySelector('#root')
 
@@ -14,7 +12,3 @@ render(
 );
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
